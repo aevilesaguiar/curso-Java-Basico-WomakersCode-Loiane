001fd0f88d6d5c5481966ac9e6808322d2c158aa
aevilesaguiar <aeviles@hotmail.com>
2022-01-29 20:00:02 -0300

    aula 07 erros

---
+++ src/br/com/aeviles/aula07/Erro2.java
@@ -0,0 +1,12 @@
+package br.com.aeviles.aula07;
+
+public class Erro2 {
+
+	public static void main (String[] args){
+
+		int cont = 0;
+
+		System.out.println("Voce digitou: " + cont);
+
+}
+}
---
+++ src/br/com/aeviles/aula07/Erro3.java
@@ -0,0 +1,11 @@
+package br.com.aeviles.aula07;
+
+public class Erro3 {
+
+	public static void main(String[] args) {
+
+		// System.out.println("Voce digitou: ");
+		System.out.println("Voce digitou: " + 1 / 0);
+
+	}
+}
